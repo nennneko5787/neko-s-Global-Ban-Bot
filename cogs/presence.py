ac885ad262235adf4742ffee668e48ecb442ec44
nennneko5787 <nennneko5787@gmail.com>
2025-04-05 19:18:44 +0900

    a

--- cogs/presence.py
+++ cogs/presence.py
@@ -5,11 +5,7 @@ from discord.ext import commands, tasks
 class PresenceCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.presenceLoop.before_loop(self.beforePresenceLoop)
         self.presenceLoop.start()
-
-    async def beforePresenceLoop(self):
-        await self.bot.wait_until_ready()
 
     @tasks.loop(seconds=20)
     async def presenceLoop(self):
